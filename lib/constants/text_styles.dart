88e38ac741c298fbc1fb42e8379e2818f65dc59e
EspeonDev <abarabash.it@gmail.com>
2025-02-20 22:24:14 +0200

    added Color and textStyles constants

---
+++ lib/constants/text_styles.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppTextStyles {
+  TextStyle firstStyle = TextStyle(
+    fontSize: 18,
+    fontFamily: 'Gilroy',
+    fontWeight: FontWeight.bold,
+  );
+}
+
+class AppColor {
+  Color mainGreenAppColor = Color(0xFF53B175);
+}
